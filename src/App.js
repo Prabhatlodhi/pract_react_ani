92cc41c73ce4f05ef38035aa231bd1a76d78c12a
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-01-16 11:44:42 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(10);
+
+  const handleClick = ()=>{
+     setCount(count + 1);
+  }
+
+
   return (
-    <div className="App">
-      
+    <div>
+      <button onClick={handleClick} >Add Animal</button>
+      <p> Number of animal is ::{count}</p>
     </div>
   );
 }
